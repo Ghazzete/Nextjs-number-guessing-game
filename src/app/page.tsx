3ce08974f1ce796804a3ccce042a09392d1500e7
Ghazzete <164881262+Ghazzete@users.noreply.github.com>
2024-10-18 10:27:47 +0500

    Number Gussing Game

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import NumberGuessing from "@/components/number-guessing";
+import React from "react";
+
+export default function Home () {
+  return (
+    <div>
+      <NumberGuessing/>
+    </div>
+  )
+}
